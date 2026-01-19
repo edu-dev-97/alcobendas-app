9a9b039945742288c4bff9305137bff3f7a1347c
edu-dev-97 <mirandaramonadrianeduardo@gmail.com>
2026-01-19 01:38:07 -0500

    18 cambio

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 //variable router para utilizar la solicitud http (GET/POST/PUT/DELETE) en express
 const router = express.Router();
-const usuarios = require('../controllers/auth.controller.js');
+const usuarios = require('../controllers/users.controller.js');
 const verifyAdmin = require('../middlewares/auth.middleware.js');
 
 router.get('/log/administrador/usuarios', verifyAdmin, usuarios);
